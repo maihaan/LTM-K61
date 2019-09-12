e8b4540d6ecf570958f3cf3ddfbf597f09c18c0d
mai ha an <maihaan24@gmail.com>
2019-09-12 13:31:31 +0700

    update 12092019

---
+++ SinhVienServer/DataAccess.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Data;
+using System.Data.SqlClient;
+
+namespace SinhVienServer
+{
+    public class DataAccess
+    {
+        // Thuoc tinh
+        String Connection;
+
+        // Phuong thuc
+        public DataAccess()
+        {
+            Connection = "Data Source=" + Properties.Settings.Default.server
+                + "; Initial catalog=" + Properties.Settings.Default.database
+                + "; User id=" + Properties.Settings.Default.userid
+                + "; Password=" + Properties.Settings.Default.password + ";";
+        }
+
+        public DataTable Read(String query)
+        {
+            SqlConnection con = new SqlConnection(Connection);
+            SqlCommand cmd = new SqlCommand(query, con);
+            DataTable tb = new DataTable();
+            try
+            {
+                con.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                tb.Load(dr, LoadOption.OverwriteChanges);
+                con.Close();
+                return tb;
+            }
+            catch
+            {
+                if (con.State == ConnectionState.Open)
+                    con.Close();
+                return null;
+            }
+        }
+
+        public int Write(String query)
+        {
+            SqlConnection con = new SqlConnection(Connection);
+            SqlCommand cmd = new SqlCommand(query, con);
+            try
+            {
+                con.Open();
+                int dem = 0;
+                dem = cmd.ExecuteNonQuery();
+                con.Close();
+                return dem;
+            }
+            catch
+            {
+                if (con.State == ConnectionState.Open)
+                    con.Close();
+                return -1;
+            }
+        }
+    }
+}
---
+++ SinhVienServer/SinhVien.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SinhVienServer
+{
+    public class SinhVien
+    {
+        public int ID { get; set; }
+        public String Ten { get; set; }
+        public String Email { get; set; }
+        public float DiemTB { get; set; }
+        public DateTime NgaySinh { get; set; }
+    }
+}
